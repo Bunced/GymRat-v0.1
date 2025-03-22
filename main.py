d4e967ff64cc15913194e341feb966a2a9c2f4e1
benjamin roseby <benjroseby@gmail.com>
2025-03-22 15:06:25 +0800

    Started new_workout function

--- main.py
+++ main.py
@@ -16,8 +16,23 @@ def main():
 """)
     print("*" * terminal_width)
     
+    1
+
+    print("Welcome to GymRat v0.1! This program is a backend proof of concept for an app for hypertrophy style gym planning and training which gamifies the process by making your digital rat friend get swole!. This verison has no such GUI or capability, and is more of a practice excersize for the developer, Benjamin Roseby.\n")
+    print("Please press the corresponding number in order to get started:")
+    print("1. Create a new workout plan.")
     
-    print("Welcome to GymRat v0.1! This program is a backend proof of concept for an app for hypertrophy style gym planning and training which gamifies the process by making your digital rat friend get swole!. This verison has no such GUI or capability, and is more of a practice excersize for the developer, Benjamin Roseby.")
-    print("Please press the corresponding number in order to get started   ")
+    match input("Your selection: "):
+        case "1":
+            new_workout()
+
+def new_workout():
     
+    days_per_week=input("Which days per week would you like to train?")
+        while True:
+            print("Please enter the days per week you would like to train, enter the letter C when done")
+            day_selection=input("")
+
 main()
+
+
